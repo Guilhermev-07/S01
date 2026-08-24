bafdcf8c6595774b3f447d1d98122af05834f5f2
Guilhermev-07 <guilhermevnr2022@gmail.com>
2026-08-24 15:20:11 -0300

    Update Exercicio01.lua

--- Relatório02/Exercicio01.lua
+++ Relatório02/Exercicio01.lua
@@ -1,1 +1,11 @@
+function gerarTabelaPotencias(inicio, fim, base)
+    for i = inicio, fim do
+        print(base .. " elevado a " .. i .. " = " .. base ^ i)
+    end
+end
 
+local M = tonumber(io.read())
+local N = tonumber(io.read())
+local B = tonumber(io.read())
+
+gerarTabelaPotencias(M, N, B)
