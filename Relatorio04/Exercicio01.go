eec18712fe97e155f278cd5d8e3f274b39ca327b
Guilhermev-07 <guilhermevnr2022@gmail.com>
2026-09-08 09:31:13 -0300

    Create Exercicio01.go

---
+++ Relatorio04/Exercicio01.go
@@ -0,0 +1,26 @@
+package main
+import "fmt"
+
+func ValidarCodigoRastreio(codigo string) (bool, string) {
+	if len(codigo) == 10 {
+		return true, "Código de rastreio registrado no sistema!"
+	}
+	return false, "Erro: O código de rastreio deve ter exatamente 10 caracteres."
+}
+
+func main() {
+	var codigo string
+
+	for {
+		fmt.Print("Digite o codigo de rastreio: ")
+		fmt.Scanln(&codigo)
+
+		valido, mensagem := ValidarCodigoRastreio(codigo)
+
+		fmt.Println(mensagem)
+
+		if valido {
+			break
+		}
+	}
+}
